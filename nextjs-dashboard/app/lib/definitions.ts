32b65f235878e4341e42582f1f2cc5bdb82cd49f
Jiri Hyvönen <jirihyvonen@gmail.com>
2024-06-10 21:47:21 +0300

    use-debounce installed

--- nextjs-dashboard/app/lib/definitions.ts
+++ nextjs-dashboard/app/lib/definitions.ts
@@ -20,8 +20,8 @@ export type Invoice = {
   id: string; // Will be created on the database
   customer_id: string;
   amount: number; // Stored in cents
-  status: 'pending' | 'paid';
   date: string;
+  status: 'pending' | 'paid';
 };
 
 export type Revenue = {
