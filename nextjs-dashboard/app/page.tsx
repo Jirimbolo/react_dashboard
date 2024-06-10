5f17596c673bd5013acca9b8cd1785daa1fb2ee2
Jiri Hyvönen <jirihyvonen@gmail.com>
2024-06-10 22:56:51 +0300

    kolmio korjattu

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,18 +1,16 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+// import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
-
-
 
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-       <div className={styles.shape} />
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">   
+  {/* <div className={styles.shape} />*/}
+    <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">   
       <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
